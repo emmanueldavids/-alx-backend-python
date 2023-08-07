6e9fb013643bd805c122bb2166040e4575cacf44
emmanueldavids <emmanueldavids417@gmail.com>
2023-08-07 15:23:11 +0000

    updated file

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,14 @@
+import asyncio
+import random
+from typing import Any
+
+async def wait_random(max_delay=10):
+    random_delay = random.uniform(0, max_delay)
+    await asyncio.sleep(random_delay)
+    return random_delay
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    loop = asyncio.get_event_loop()
+    coro = wait_random(max_delay)  # Import and call the wait_random function here
+    task = loop.create_task(coro)
+    return task
