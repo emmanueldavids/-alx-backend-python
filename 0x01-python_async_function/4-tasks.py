dc1344ba97979894a98f3d35dec9b677a64f104c
emmanueldavids <emmanueldavids417@gmail.com>
2023-08-08 14:27:28 +0000

    updated the files

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,11 +1,21 @@
 #!/usr/bin/env python3
+""" Take the code from wait_n and alter it into a new function
+    task_wait_n. The code is nearly identical to wait_n except
+    task_wait_random is being called.
+"""
+
 
 import asyncio
 from typing import List
-from 0-basic_async_syntax import task_wait_random
+wait_n = __import__('0-basic_async_syntax').wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    tasks = [task_wait_random(max_delay) for _ in range(n)]
-    delays = await asyncio.gather(*tasks)
-    return sorted(delays)
+    """ created a new asynchronous function task_wait_random
+        that returns the result of the wait_random coroutine
+        after awaiting the task.
+    """
+
+    tasks = [wait_n(max_delay) for _ in range(n)]
+    results = await asyncio.gather(*tasks)
+    return results
