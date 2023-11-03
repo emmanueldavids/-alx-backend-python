a4f9e0d21063ce1a89da9f0a36738f699b9e193f
emmanueldavids <emmanueldavids417@gmail.com>
2023-11-03 16:22:15 +0000

    added files

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -6,4 +6,13 @@
 
 
 def add(a: float, b: float) -> float:
+    """
+        Args:
+            a: The first parameter
+            b: The second parameter
+
+        Return:
+            Sum of two floats
+
+    """
     return a + b
